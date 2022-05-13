9880b546b3a0508c1e57d5a22e5dec214fe770b5
CHAO ZHOU <44282719+hatlabcz@users.noreply.github.com>
2022-05-13 14:38:55 -0400

    rewrite custom gate class

--- custom_gates.py
+++ custom_gates.py
@@ -1,7 +1,7 @@
 import numpy as np
 from qiskit.circuit.gate import Gate
 from qiskit.circuit.parameterexpression import ParameterValueType
-
+import qutip
 """Example Usage:
 from custom_gates import *
 from qiskit import QuantumCircuit
@@ -9,7 +9,50 @@ qc = QuantumCircuit(2)
 qc.append(RiSwapGate(1/4), [0,1])
 qc.draw(output='mpl')
 """
+def build_circulator_U(
+    phi_ab=0, phi_ac=0, phi_bc=np.pi / 2, g_ab=1.0, g_ac=1.0, g_bc=1.0
+):
+    # TODO: when sweeping phase parameters leave time-independent
+    # when sweeping coupling parameters make time-dependet i.e gaussian shaped pusle
+    # build raising and lowering operations
+    a = qutip.operators.create(N=2)
+    I2 = qutip.operators.identity(2)
+    A = qutip.tensor(a, I2, I2)
+    B = qutip.tensor(I2, a, I2)
+    C = qutip.tensor(I2, I2, a)
 
+    # construct circulator Hamiltonian
+    H_ab = np.exp(1j * phi_ab) * A * B.dag() + np.exp(-1j * phi_ab) * A.dag() * B
+    H_ac = np.exp(1j * phi_ac) * A * C.dag() + np.exp(-1j * phi_ac) * A.dag() * C
+    H_bc = np.exp(1j * phi_bc) * B * C.dag() + np.exp(-1j * phi_bc) * B.dag() * C
+    H = g_ab * H_ab + g_ac * H_ac + g_bc * H_bc
+
+    # time evolution, if time dependent need to use master-equation
+    # qutip.mesolve()
+    # U = expm(1j*np.array(H))
+    U = (1j * H).expm()
+    return U
+
+v_nn = np.sqrt(2)*np.pi/np.arccos(1/np.sqrt(3))
+v_params = [np.pi/2,np.pi/2,0, np.pi/v_nn, np.pi/v_nn, 0]
+_Vswap_array = build_circulator_U(*v_params)
+
+nn= 3*np.sqrt(3)/2
+params = [-np.pi/2, np.pi/2, -np.pi / 2, np.pi/nn, np.pi/nn, np.pi/nn]
+_Cparity_array = build_circulator_U(*params)
+
+class VSwap(Gate):
+    def __init__(self):
+        super().__init__("vswap", 3, [], "VSWAP")
+        v_nn = np.sqrt(2)*np.pi/np.arccos(1/np.sqrt(3))
+        v_params = [np.pi/2,np.pi/2,0, np.pi/v_nn, np.pi/v_nn, 0]
+        self._array = build_circulator_U(*v_params)
+        
+    def __array__(self, dtype=None):
+        # v_nn = np.sqrt(2)*np.pi/np.arccos(1/np.sqrt(3))
+        # params = [np.pi/2,np.pi/2,0, np.pi/v_nn, np.pi/v_nn, 0]
+        # U = build_circulator_U(*params)
+        return self._array.full()
 
 class CParitySwap(Gate):
     r"""Yet to be determined 3Q gate"""
