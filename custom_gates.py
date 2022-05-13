da137de86e9cb2b85b32026af8cb81b10d188e6f
Evan McKinney <evmckinney9@gmail.com>
2022-05-13 14:58:59 -0400

    add new CX templates for CParitySwap

--- custom_gates.py
+++ custom_gates.py
@@ -2,6 +2,7 @@ import numpy as np
 from qiskit.circuit.gate import Gate
 from qiskit.circuit.parameterexpression import ParameterValueType
 import qutip
+
 """Example Usage:
 from custom_gates import *
 from qiskit import QuantumCircuit
@@ -9,6 +10,8 @@ qc = QuantumCircuit(2)
 qc.append(RiSwapGate(1/4), [0,1])
 qc.draw(output='mpl')
 """
+
+
 def build_circulator_U(
     phi_ab=0, phi_ac=0, phi_bc=np.pi / 2, g_ab=1.0, g_ac=1.0, g_bc=1.0
 ):
@@ -33,26 +36,49 @@ def build_circulator_U(
     U = (1j * H).expm()
     return U
 
-v_nn = np.sqrt(2)*np.pi/np.arccos(1/np.sqrt(3))
-v_params = [np.pi/2,np.pi/2,0, np.pi/v_nn, np.pi/v_nn, 0]
+
+v_nn = np.sqrt(2) * np.pi / np.arccos(1 / np.sqrt(3))
+v_params = [np.pi / 2, np.pi / 2, 0, np.pi / v_nn, np.pi / v_nn, 0]
 _Vswap_array = build_circulator_U(*v_params)
 
-nn= 3*np.sqrt(3)/2
-params = [-np.pi/2, np.pi/2, -np.pi / 2, np.pi/nn, np.pi/nn, np.pi/nn]
+nn = 3 * np.sqrt(3) / 2
+params = [-np.pi / 2, np.pi / 2, -np.pi / 2, np.pi / nn, np.pi / nn, np.pi / nn]
 _Cparity_array = build_circulator_U(*params)
+
 
 class VSwap(Gate):
     def __init__(self):
         super().__init__("vswap", 3, [], "VSWAP")
-        v_nn = np.sqrt(2)*np.pi/np.arccos(1/np.sqrt(3))
-        v_params = [np.pi/2,np.pi/2,0, np.pi/v_nn, np.pi/v_nn, 0]
+        v_nn = np.sqrt(2) * np.pi / np.arccos(1 / np.sqrt(3))
+        v_params = [np.pi / 2, np.pi / 2, 0, np.pi / v_nn, np.pi / v_nn, 0]
         self._array = build_circulator_U(*v_params)
-        
+
     def __array__(self, dtype=None):
         # v_nn = np.sqrt(2)*np.pi/np.arccos(1/np.sqrt(3))
         # params = [np.pi/2,np.pi/2,0, np.pi/v_nn, np.pi/v_nn, 0]
         # U = build_circulator_U(*params)
         return self._array.full()
+
+
+class Peres(Gate):
+    def __init__(self, _: ParameterValueType = None):
+        super().__init__("peres", 3, [], "PERES")
+
+    def __array__(Self, dtype=None):
+        return np.array(
+            [
+                [1, 0, 0, 0, 0, 0, 0, 0],
+                [0, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 1, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1],
+                [0, 0, 0, 0, 0, 0, 1, 0],
+                [0, 0, 0, 0, 0, 1, 0, 0],
+                [0, 0, 0, 0, 1, 0, 0, 0],
+            ],
+            dtype=dtype,
+        )
+
 
 class CParitySwap(Gate):
     r"""Yet to be determined 3Q gate"""
