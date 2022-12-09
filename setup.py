8398591ebd2029a036efbe4981756e8e70d7be33
Evan McKinney <evmckinney9@gmail.com>
2022-12-09 11:53:56 -0500

    package refactoring #7

--- setup.py
+++ setup.py
@@ -1,5 +1,28 @@
-# from setuptools import setup, find_packages
-# setup(
-#     name = 'decompem',
-#     packages = find_packages("src"),
-# )
+from setuptools import setup, find_packages
+import io
+
+name = 'slam'
+description = 'Parallel Driving for Fast Quantum Computing Under Speed Limits'
+
+# README file as long_description.
+long_description = io.open("README.md", encoding="utf-8").read()
+
+# Read in requirements
+requirements = open("requirements.txt").readlines()
+requirements = [r.strip() for r in requirements]
+
+packages = find_packages()
+print(packages)
+
+setup(
+    name=name,
+    version="0.0.1",
+    author="Evan McKinney",
+    author_email="evm33@pitt.edu",
+    python_requires=(">=3.7.0"),
+    install_requires=requirements,
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=packages
+)
